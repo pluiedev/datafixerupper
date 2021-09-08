4af31aa1c7ce90c34e66a4bdfec00ae4dc12483e
leocth <22406910+LeoCTH@users.noreply.github.com>
2021-09-09 07:54:33 +0800

    kinds for some common types in std

--- src/datafixers/kinds/mod.rs
+++ src/datafixers/kinds/mod.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 mod generated;
+use std::collections::*;
+
 // reexport
 pub use self::generated::*;
 
-struct Pepperoni<A,B,C,D> {
-    a: A,
-    b: B,
-    c: C,
-    d: D
-}
-
-kind4!(Pepperoni);
+kind1!(Vec);
+kind1!(VecDeque);
+kind1!(HashSet);
+kind1!(BTreeSet);
+kind1!(Option);
+kind2!(Result);
+kind2!(HashMap);
+kind2!(BTreeMap);
